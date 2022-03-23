c3978914a79a9ad71e5313eb16a4bba7492913c3
oGabrielDev <gabriel.thiago611@gmail.com>
2022-03-23 16:23:49 -0300

    Ajustando algumas configuraçoes

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const btnLimpar = document.getElementById('clear-board');
 const btnBoard = document.getElementById('generate-board');
 
+// Como fazer numeros aleatorios para gerar o RGB => https://www.w3schools.com/js/js_random.asp.
+// template string aprendi no curso do Guanabara.
 function randomColor() {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
   const cor = `rgb(${r}, ${g}, ${b})`;
   return cor;
+}
+// Como Selecionar a propriedade https://www.w3schools.com/jsref/event_target.asp.
+function selectionColor(event) {
+  const colorSelection = document.getElementsByClassName('selected');
+  colorSelection[0].classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
 function criarPaleta(colors) {
@@ -21,30 +29,9 @@ function criarPaleta(colors) {
       newColor.classList = 'color';
       newColor.style.backgroundColor = randomColor();
     }
-    newColor.addEventListener('click', selectionColor);
     pixels.appendChild(newColor);
+    newColor.addEventListener('click', selectionColor);
   }
-}
-
-function linhas(tamanho) {
-  for (let i = 0; i < tamanho; i += 1) {
-    const pixel = document.getElementById('pixel-board');
-    const pixelOne = document.createElement('div');
-    pixel.appendChild(pixelOne);
-    for (let n = 0; n < tamanho; n += 1) {
-      const box = document.createElement('div');
-      box.classList = 'pixel';
-      pixelOne.appendChild(box);
-      box.addEventListener('click', paintPixel);
-    }
-  }
-}
-linhas(5);
-
-function selectionColor(event) {
-  const colorSelection = document.getElementsByClassName('selected');
-  colorSelection[0].classList.remove('selected');
-  event.target.classList.add('selected');
 }
 
 function paintPixel(event) {
@@ -60,8 +47,46 @@ function paintPixel(event) {
   }
 }
 
+function linhas(tamanho) {
+  const pixel = document.getElementById('pixel-board');
+  pixel.innerHTML = '';
+  let box;
+  const tamanhoMaximo = (tamanho + 1) * 40;
+  pixel.style.maxWidth = `${tamanhoMaximo}px`;
+  for (let i = 0; i < tamanho; i += 1) {
+    for (let n = 0; n < tamanho; n += 1) {
+      box = document.createElement('div');
+      box.classList = 'pixel';
+      pixel.appendChild(box);
+      box.addEventListener('click', paintPixel);
+    }
+  }
+}
+
+function clear() {
+  const pixel = document.getElementsByClassName('pixel');
+  for (let n = 0; n < pixel.length; n += 1) {
+    pixel[n].style.backgroundColor = 'white';
+  }
+}
+
+function input() {
+  if (document.getElementById('board-size').value === '') {
+    alert('Board inválido!');
+  }
+  let tamanho = parseInt(document.getElementById('board-size').value, 10);
+  if (tamanho < 5) {
+    tamanho = 5;
+    alert('tamanho incorreto, executando com o valor minimo 5.');
+  } else if (tamanho > 50) {
+    tamanho = 50;
+    alert('Tamanho Maximo 50, executando com o valor maximo 50.');
+  }
+  linhas(tamanho);
+}
+
 criarPaleta(4);
+linhas(5);
 
-// btnLimpar.addEventListener('click', limpaPixels);
-
-// btnBoard.addEventListener('click', validaInput);
+btnLimpar.addEventListener('click', clear);
+btnBoard.addEventListener('click', input);
